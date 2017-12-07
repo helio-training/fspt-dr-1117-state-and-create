83f95365989f88e8da39cd8fb5fbaa24e36c4651
Danny Warren <dannydwarren@gmail.com>
2017-12-06 20:15:33 -0700

    Add switch

--- src/TestFunctions.js
+++ src/TestFunctions.js
@@ -29,17 +29,20 @@ const employees = [
     {
         name: "Danny Warren",
         salary: 10000000,
-        payPeriod: "year"
+        payPeriod: "day",
+        tenureInYears: 10
     },
     {
         name: "Tim",
         salary: 100.50,
-        payPeriod: "day"
+        payPeriod: "day",
+        tenureInYears: 0
     },
     {
         name: "Andy",
         salary: 25.50,
-        payPeriod: "month"
+        payPeriod: "month",
+        tenureInYears: 0
     }
 ]
 
@@ -47,33 +50,50 @@ console.log("employees", employees)
 console.log("employee index 3", employees[3])
 
 const processPayroll = (employee) => {
-    console.log(employee)
+    console.log("if", employee)
     if (employee.payPeriod === "month") {
         return employee.salary * 12
     } else if (employee.payPeriod === "week") {
         return employee.salary * 52
-    } else if (employee.payPeriod === "day") {
+    } else if (employee.payPeriod === "day" && employee.tenureInYears < 10) {
         return employee.salary * 365
     }
     return employee.salary
 }
 
-for (const i in employees) {
-    console.log(i % 2 === 0 ? "" : "Other")
-    const yearlySalary = processPayroll(employees[i])
-    console.log(employees[i].name, yearlySalary)
+const processPayrollSwitch = (employee) => {
+    console.log("switch", employee)
+    switch(employee.payPeriod) {
+        case "month":
+            return employee.salary * 12
+        case "week":
+            return employee.salary * 52
+        case "daily":
+        case "day": 
+            return employee.tenureInYears < 10 
+                        ? employee.salary * 365 
+                        : employee.salary * 365 * 10
+        default:
+            return employee.salary
+    }
 }
+
+// for (const i in employees) {
+//     console.log(i % 2 === 0 ? "" : "Other")
+//     const yearlySalary = processPayroll(employees[i])
+//     console.log(employees[i].name, yearlySalary)
+// }
 
 for (const employee of employees) {
     const yearlySalary = processPayroll(employee)
     console.log(employee.name, yearlySalary)
 }
 
-let i = 0
-console.log("While starts after this: ", i)
-while (i < employees.length) {
-    const yearlySalary = processPayroll(employees[i])
-    console.log("while i is " + i, employees[i].name, yearlySalary)
-    i++    
-}
+// let i = 0
+// console.log("While starts after this: ", i)
+// while (i < employees.length) {
+//     const yearlySalary = processPayroll(employees[i])
+//     console.log("while i is " + i, employees[i].name, yearlySalary)
+//     i++    
+// }
 
